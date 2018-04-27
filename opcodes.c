f3f8acd658a4e80641e0397a883b06c2b0492b0b
Syssos <Syssos@gitgub.com>
2018-04-27 14:06:49 -0700

    updated main

--- opcodes.c
+++ opcodes.c
@@ -97,5 +97,5 @@ void c_pop(stack_t **stack, unsigned int line_number)
  */
 void c_nop(__attribute__ ((unused))stack_t **stack, __attribute__((unused))unsigned int line_number)
 {
-	return;
+        ;
 }
