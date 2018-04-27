992c95e63aa91a3f4a155bc130bd2850ddbad86a
Syssos <Syssos@gitgub.com>
2018-04-27 14:04:52 -0700

    updated main

--- opcodes.c
+++ opcodes.c
@@ -84,7 +84,7 @@ void c_pop(stack_t **stack, unsigned int line_number)
 {
     if (*stack == NULL)
     {
-        printf("L%d: can't pop an empty stack", line_number);
+        printf("L%d: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
     delete_dnodeint_at_index(stack, 0);
